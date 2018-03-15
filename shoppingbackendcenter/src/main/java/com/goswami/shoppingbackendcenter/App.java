9f82d874ed5cfb53b42cfdb955f8aa81eca8bf9d
Aneel <Aneel@Goswami>
2018-03-15 09:53:44 +0500

    My complete project success

---
+++ shoppingbackendcenter/src/main/java/com/goswami/shoppingbackendcenter/App.java
@@ -0,0 +1,13 @@
+package com.goswami.shoppingbackendcenter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
